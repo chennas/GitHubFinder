dddd2fe2ef8e56c7735ca36e4711e5493f74f2dc
sivakumar <ckumarreddy@yahoo.com>
2017-02-25 20:35:31 +0530

    Complete application

---
+++ GitHubFinder/scripts/main.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+    $('#searchUser').on('keyup', function (e) {
+        let username = e.target.value;
+
+        //make request to GitHub
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            data: {
+                client_id: '7da32b82458a2bd22623',
+                client_secret: '9121fe061f193d1259620e175621ee6c9873024f'
+            }
+        }).done(function (user) {
+            $.ajax({
+                url: 'https://api.github.com/users/' + username + '/repos',
+                data: {
+                    client_id: '7da32b82458a2bd22623',
+                    client_secret: '9121fe061f193d1259620e175621ee6c9873024f',
+                    sort: 'created: asc' //,
+                    //per_page: 5
+                }
+            }).done(function (repos) {
+                $('#repos').html('');
+                $.each(repos, function(index, repo) {
+                    $('#repos').append(`
+                        <div class ="well">
+                            <div class ="row">
+                                <div class ="col-md-7">
+                                    <strong>${repo.name}</strong>: ${repo.description}
+                                </div>
+                                <div class ="col-md-3">
+                                    <span class ="label label-default">Forks: ${repo.forks_count}</span>
+                                    <span class ="label label-primary">Watchers: ${repo.watchers_count}</span>
+                                    <span class ="label label-success">Stars: ${repo.stargazers_count}</span>
+                                </div>
+                                <div class ="col-md-2">
+                                    <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+                                </div>
+                            </div>
+                        </div>
+                  `)
+                });
+            });
+            $('#profile').html(`
+                <div class ="panel panel-default">
+                  <div class ="panel-heading">
+                    <h3 class ="panel-title">${user.name}</h3>
+                  </div>
+                  <div class ="panel-body">
+                    <div class ="row">
+                        <div class="col-md-3">
+                            <img class ="thumbnail avatar" src="${user.avatar_url}">
+                            <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                        </div>
+                        <div class="col-md-9">
+                            <span class ="label label-default">Public Repos: ${user.public_repos}</span>
+                            <span class ="label label-primary">Public Gists: ${user.public_gists}</span>
+                            <span class ="label label-success">Followers: ${user.followers}</span>
+                            <span class ="label label-info">Following: ${user.following}</span>
+                            <br/><br/>
+                            <ul class ="list-group">
+                                <li class ="list-group-item">Company: ${user.company}</li>
+                                <li class ="list-group-item">Website/Blog: <a target="_blank" href="https://${user.blog}">${user.blog}</a></li>
+                                <li class ="list-group-item">Location: ${user.location}</li>
+                                <li class ="list-group-item">Member Since: ${user.created_at}</li>
+                                <li class ="list-group-item">Biography: ${user.bio}</li>
+                            </ul>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <h3 class ="page-header">Latest Repos</h3>
+                <div id="repos"></div>
+              `);
+        });
+    });
+});
